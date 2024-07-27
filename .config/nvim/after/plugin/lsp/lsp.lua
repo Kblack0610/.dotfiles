5631694b5ebe889bf725abfd862a7333c276db1b
Kenneth <kblack0610@gmail.com>
2024-07-26 17:32:06 -0700

    more added

--- .config/nvim/after/plugin/lsp/lsp.lua
+++ .config/nvim/after/plugin/lsp/lsp.lua
@@ -140,3 +140,33 @@ require("mason-lspconfig").setup_handlers({
 	end,
 })
 
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  -- Omnicompletion
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  local opts = { noremap=true, silent=true }
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', 'gR', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+end
+
+-- Omnisharp/C#/Unity
+local pid = vim.fn.getpid()
+local omnisharp_bin = "/opt/omnisharp-roslyn/run"
+require'lspconfig'.omnisharp.setup{
+    on_attach = on_attach,
+    flags = {
+      debounce_text_changes = 150,
+    },
+    cmd = { omnisharp_bin, "--languageserver" , "--hostPID", tostring(pid) };
+}
