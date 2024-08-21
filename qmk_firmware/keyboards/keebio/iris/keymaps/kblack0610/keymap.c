3a1c6f3143b4cb40c90bc086b225fe25e5362b1a
Kenneth <kblack0610@gmail.com>
2024-08-21 14:10:48 -0700

    made some cleanups and fixes to keyboard and others

--- qmk_firmware/keyboards/keebio/iris/keymaps/kblack0610/keymap.c
+++ qmk_firmware/keyboards/keebio/iris/keymaps/kblack0610/keymap.c
@@ -1,18 +1,19 @@
+/* this should define the tapping term , unsure if its working*/
+#define TAPPING_TERM 130
 #include QMK_KEYBOARD_H
-
 
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
 #define _ADJUST 3
-#define _GAMING 4
+/* #define _GAMING 4 */
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
   ADJUST,
-  GAMING,
+  /* GAMING, */
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -55,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_MUTE, KC_CIRC, KC_PERC, KC_LBRC, KC_RBRC, KC_TILD, KC_HOME,          _______,  _______, KC_DQUO,  KC_QUOT, KC_GRV, KC_BSLS, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_S, ADJUST,  KC_LALT,                   TO(GAMING), _______, _______
+                                    KC_S, ADJUST,  KC_LALT,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -73,86 +74,133 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
  ),
 
- [_GAMING] = LAYOUT(
-  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
-  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     ADJUST, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_QUOT,
-  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, MT(MOD_LCTL, KC_Z), KC_X,    KC_C,    KC_V,    KC_B,    KC_HOME,          KC_ESC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SCLN,
-  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LGUI,  KC_SPC,   MOD_LCTL,     KC_ENT,  KC_SPC , TO(QWERTY)
-                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
-  ),
+ /* [_GAMING] = LAYOUT( */
+ /*  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐ */
+ /*     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, */
+ /*  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤ */
+ /*     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL, */
+ /*  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤ */
+ /*     ADJUST, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN, KC_QUOT, */
+ /*  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤ */
+ /*     KC_LSFT, MT(MOD_LCTL, KC_Z), KC_X,    KC_C,    KC_V,    KC_B,    KC_HOME,          KC_ESC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SCLN, */
+ /*  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘ */
+ /*                                    KC_LGUI,  KC_SPC,   MOD_LCTL,     KC_ENT,  KC_SPC , TO(QWERTY) */
+ /*                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘ */
+ /*  ), */
 
 };
 
- bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {               // Index 0 is the first encoder
-        if (clockwise) {
-            tap_code(KC_WH_D);      // Page Down is sent if the 2nd encoder is rotated clockwise
-        } else {
-            tap_code(KC_WH_U);      // Page Up is sent if the 2nd encoder is rotated counter-clockwise
-        }
-    }
-    else if (index == 1) {          // Index 1 is the second encoder
-        if (clockwise) {
-            tap_code(KC_PGDN);      // Page Down is sent if the 2nd encoder is rotated clockwise
-        } else {
-            tap_code(KC_PGUP);      // Page Up is sent if the 2nd encoder is rotated counter-clockwise
-        }
-    }
-
-    // If you have more encoders, add more `else if (index == ...)` blocks here
-
-    return false;
-}
-
+/*  bool encoder_update_user(uint8_t index, bool clockwise) { */
+/*     if (index == 0) {               // Index 0 is the first encoder */
+/*         if (clockwise) { */
+/*             tap_code(KC_WH_D);      // Page Down is sent if the 2nd encoder is rotated clockwise */
+/*         } else { */
+/*             tap_code(KC_WH_U);      // Page Up is sent if the 2nd encoder is rotated counter-clockwise */
+/*         } */
+/*     } */
+/*     else if (index == 1) {          // Index 1 is the second encoder */
+/*         if (clockwise) { */
+/*             tap_code(KC_PGDN);      // Page Down is sent if the 2nd encoder is rotated clockwise */
+/*         } else { */
+/*             tap_code(KC_PGUP);      // Page Up is sent if the 2nd encoder is rotated counter-clockwise */
+/*         } */
+/*     } */
+/**/
+/*     // If you have more encoders, add more `else if (index == ...)` blocks here */
+/**/
+/*     return false; */
+/* } */
+/**/
+/* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
+/*   switch (keycode) { */
+/*     case qwerty: */
+/*       if (record->event.pressed) { */
+/*         set_single_persistent_default_layer(_qwerty); */
+/*       } */
+/*       return false; */
+/*       break; */
+/*     case lower: */
+/*       if (record->event.pressed) { */
+/*         layer_on(_lower); */
+/*         update_tri_layer(_lower, _raise, _adjust); */
+/*       } else { */
+/*         layer_off(_lower); */
+/*         update_tri_layer(_lower, _raise, _adjust); */
+/*       } */
+/*       return false; */
+/*       break; */
+/*     case raise: */
+/*       if (record->event.pressed) { */
+/*         layer_on(_raise); */
+/*         update_tri_layer(_lower, _raise, _adjust); */
+/*       } else { */
+/*         layer_off(_raise); */
+/*         update_tri_layer(_lower, _raise, _adjust); */
+/*       } */
+/*       return false; */
+/*       break; */
+/*     case adjust: */
+/*       if (record->event.pressed) { */
+/*         layer_on(_adjust); */
+/*       } else { */
+/*         layer_off(_adjust); */
+/*       } */
+/*       return false; */
+/*      case gaming: */
+/*       if (record->event.pressed) { */
+/*         layer_on(_adjust); */
+/*       } else { */
+/*         layer_off(_adjust); */
+/*       } */
+/*       return false; */
+/*       break; break; */
+/*   } */
+/*   return true; */
+/* } */
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case qwerty:
+    case QWERTY:
       if (record->event.pressed) {
-        set_single_persistent_default_layer(_qwerty);
+        set_single_persistent_default_layer(_QWERTY);
       }
       return false;
       break;
-    case lower:
+    case LOWER:
       if (record->event.pressed) {
-        layer_on(_lower);
-        update_tri_layer(_lower, _raise, _adjust);
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
-        layer_off(_lower);
-        update_tri_layer(_lower, _raise, _adjust);
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
       break;
-    case raise:
+    case RAISE:
       if (record->event.pressed) {
-        layer_on(_raise);
-        update_tri_layer(_lower, _raise, _adjust);
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
-        layer_off(_raise);
-        update_tri_layer(_lower, _raise, _adjust);
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
       break;
-    case adjust:
+    case ADJUST:
       if (record->event.pressed) {
-        layer_on(_adjust);
+        layer_on(_ADJUST);
       } else {
-        layer_off(_adjust);
+        layer_off(_ADJUST);
       }
       return false;
-     case gaming:
-      if (record->event.pressed) {
-        layer_on(_adjust);
-      } else {
-        layer_off(_adjust);
-      }
-      return false;
-      break; break;
+      break;
+     /* case GAMING: */
+     /*  if (record->event.pressed) { */
+     /*    layer_on(_ADJUST); */
+     /*  } else { */
+     /*    layer_off(_ADJUST); */
+     /*  } */
+     /*  return false; */
+     /*  break; break; */
   }
   return true;
 }
--- scratch.md
+++ scratch.md
@@ -4,7 +4,7 @@
 
 - figure out what the fuck you want to do with home assistant and be done with it. []
         - setup nighthawk [x]
-        - add openwrt to nighthawk []
+        - add openwrt to nighthawk [x]
         - use openwrt to set up subnets for devices []
 
 - taxes/quickbooks []
@@ -26,14 +26,21 @@ ITS BEAUTIFUL
 -have full animation folder on SO[x]
 -fuggedaboudit, would make the game a hory shit gewd.
 
-- readd pickup[]
+-- going to need movement, action, and condition state machines 
+
+//REMOVE MOST OF THIS AND THEN REWRITE IT ABOUT ORGANIZING AND REMOVING ALL BULLSHIT.
+
+- readd original functions (run, quit, crouch, jump, etc.)
+- readd pickup [x]
 - readd throw[]
 - readd scene view[]
 - readd blend trees[]
     - roll
-    - idle
+    - idle [x]
 - remove mecanim completely [x]
+- remove behaviors completely [x]
 
+-need to look at animal controller again for how it handles actions, states(seems like just movement), and stances. can probably do a smooth simple vreesion of this similar to how I was originall thinking.
 - new AI MOTHAFUCKA 
 
 - add powerup balls[]
@@ -43,7 +50,5 @@ ITS BEAUTIFUL
 # fun:
 
 - finish soldering keyboard [] 
-
 - buy piano motherfucker (feel the music) []
-
-- brew motha fucka
+- brew motha fucka[x]
