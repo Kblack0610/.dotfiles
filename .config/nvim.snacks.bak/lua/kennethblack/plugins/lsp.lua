8335e79f754a536af53d48e4e3d08776ae3e830e
Kenneth <kblack0610@gmail.com>
2025-02-27 19:54:40 -0800

    moving snacks config to bak so I can work on it later

---
+++ .config/nvim.snacks.bak/lua/kennethblack/plugins/lsp.lua
@@ -0,0 +1,110 @@
+local ensure_installed = {
+  "lua_ls",
+  "ts_ls",
+  "jsonls",
+  "bashls",
+  "html",
+  "cssls",
+  "omnisharp",
+  "sqlls",
+  -- "gopls",
+  "yamlls",
+  "pyright",
+  "emmet_language_server",
+  "angularls"
+}
+
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {},
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = ensure_installed,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    -- Remove if you want to go back to nvim-cmp
+    dependencies = { "saghen/blink.cmp" },
+    config = function()
+      local lspconfig = require "lspconfig"
+      -- If you want to go back to nvim-cmp
+      --   local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
+      require("lspconfig.ui.windows").default_options.border = "rounded"
+
+      -- Set border for shift+k
+      -- also ignore "no information found" when multiple lsps attached and trying hover
+      vim.lsp.handlers["textDocument/hover"] = function(_, result, ctx, config)
+        if not (result and result.contents and result.contents.value ~= "") then
+          return -- Suppress "no information available" notifications
+        end
+        -- merge config table if it exists
+        return vim.lsp.handlers.hover(_, result, ctx, vim.tbl_extend("force", config or {}, { border = "rounded" }))
+      end
+      -- Set border for signature help <leader>lh
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+        border = "rounded",
+      })
+
+      -- Setup up all language servers that are installed
+      for _, value in ipairs(ensure_installed) do
+        if value == "ts_ls" then
+          lspconfig[value].setup {
+            capabilities = capabilities,
+            on_attach = function(client, _)
+              -- Disable formatting capability for tsserver
+              -- This conflicts with other formatters
+              client.server_capabilities.documentFormattingProvider = false
+              client.server_capabilities.documentRangeFormattingProvider = false
+            end,
+          }
+        elseif value == "emmet_language_server" then
+          lspconfig[value].setup {
+            capabilities = capabilities,
+            filetypes = { "html", "css", "scss", "javascriptreact", "typescriptreact", "javascript" },
+          }
+        else
+          lspconfig[value].setup {
+            capabilities = capabilities,
+          }
+        end
+      end
+
+      -- lint/formatters
+      -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#eslint
+      -- NOTE: need to install npm install -g vscode-langservers-extracted (eslint globally) for lsp to work
+      -- eslint also needs to be installed locally in js/ts project
+      -- this is ONLY for eslint projects
+      lspconfig.eslint.setup {
+        settings = {
+          format = false,
+        },
+      }
+      lspconfig.biome.setup {}
+
+      -- key bindings
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("LspKeybindings", { clear = true }),
+        callback = function(event)
+          local opts = { buffer = event.buf }
+
+          -- this is mapped in snack.picker
+          -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          -- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "<leader>lR", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>lh", vim.lsp.buf.signature_help, opts)
+          vim.keymap.set("n", "gK", vim.lsp.buf.signature_help, opts)
+          vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, opts)
+          -- using conform now
+          -- vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format { async = true } end, opts)
+          --
+        end,
+      })
+    end,
+  },
+}
---
+++ .config/nvim.snacks.bak/lua/kennethblack/plugins/snacks.lua
@@ -0,0 +1,137 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+        dashboard = { enabled = true },
+        picker = {
+            enabled = true,
+            win = {
+                input = {
+                    keys = {
+                        ["<a-s>"] = { "flash", mode = { "n", "i" } },
+                        ["s"] = { "flash" },
+                    },
+                },
+            },
+        },
+        notifier = {
+            enabled = true
+        },
+        bigfile = { enabled = true },
+        debug = { enabled = true },
+        gitbrowse = { enabled = true },
+        dim = {
+            -- only enable when we toggle it on
+            enabled = false,
+            animate = {
+                -- dont animate
+                enabled = false,
+            },
+        },
+        indent = {
+            enabled = true,
+            animate = {
+                enabled = false
+            },
+            scope = {
+                -- Current scope indent highlight with color toggle
+                -- dont animate currently
+                enabled = false
+            }
+        },
+        -- image only works for .png without magick installed
+        image = { enabled = true },
+        lazygit = {
+            configure = true,
+
+        },
+
+        --
+        -- Snack plugin styles
+        --
+        styles = {
+            lazygit = {
+                width = 0.99,
+                height = 0.99,
+                border = "rounded",
+            }
+        }
+    },
+    keys = {
+        -- notifier
+        { "<leader>un", function() Snacks.notifier.hide() end,                                                                               desc = "Dismiss All Notifications" },
+        -- gitbrowse
+        -- CURRENTLY must use gitblame and hover commit hash, then enter command
+        { "<leader>gc", function() Snacks.gitbrowse.open({ what = 'commit' }) end,                                                           desc = "Open commit in browser",   mode = { "n", "v" } },
+        -- spell(override default with picker for spelling)
+        { "z=",         function() Snacks.picker.spelling() end,                                                                             desc = "Open commit in browser",   mode = { "n", "v" } },
+        --
+        -- pickers
+        --
+        -- file: https://github.com/folke/snacks.nvim/blob/main/docs/picker.md#files
+        { "<leader>pf", function() Snacks.picker.files({ hidden = true, ignored = true, exclude = { "node_modules", "dist" } }) end,         desc = "Find Files" },
+        { "<leader>pg", function() Snacks.picker.grep({ hidden = true, ignored = true, exclude = { "node_modules", "dist" } }) end,          desc = "Grep" },
+        { "<leader>pb", function() Snacks.picker.recent() end,                                                                               desc = "Recent" },
+        { "<leader>pt", function() Snacks.picker.todo_comments({ hidden = true, ignored = true, exclude = { "node_modules", "dist" } }) end, desc = "Todo" },
+        { "<leader>pn", function() Snacks.picker.notifications() end,                                                                        desc = "Notification History" },
+
+        -- lazygit
+        { "<leader>gg", function() Snacks.lazygit() end,                                                                                     desc = "Lazygit" },
+        -- git
+        { "<leader>gb", function() Snacks.picker.git_branches() end,                                                                         desc = "Git Branches" },
+        { "<leader>gL", function() Snacks.picker.git_log() end,                                                                              desc = "Git Log" },
+        { "<leader>gf", function() Snacks.picker.git_log_file() end,                                                                         desc = "Git Log File" },
+        { "<leader>gs", function() Snacks.picker.git_status() end,                                                                           desc = "Git Status" },
+        { "<leader>gS", function() Snacks.picker.git_stash() end,                                                                            desc = "Git Stash" },
+        { "<leader>gd", function() Snacks.picker.git_diff() end,                                                                             desc = "Git Diff (Hunks)" },
+        -- search
+        { "<leader>sk", function() Snacks.picker.keymaps() end,                                                                              desc = "Keymaps" },
+        { '<leader>s"', function() Snacks.picker.registers() end,                                                                            desc = "Registers" },
+        { '<leader>s/', function() Snacks.picker.search_history() end,                                                                       desc = "Search History" },
+        { "<leader>sc", function() Snacks.picker.command_history() end,                                                                      desc = "Command History" },
+        { "<leader>sC", function() Snacks.picker.commands() end,                                                                             desc = "Commands" },
+        { "<leader>lD", function() Snacks.picker.diagnostics_buffer() end,                                                                   desc = "Buffer Diagnostics" },
+        { "<leader>sD", function() Snacks.picker.diagnostics() end,                                                                          desc = "Diagnostics" },
+        { "<leader>sh", function() Snacks.picker.help() end,                                                                                 desc = "Help Pages" },
+        { "<leader>sH", function() Snacks.picker.highlights() end,                                                                           desc = "Highlights" },
+        { "<leader>si", function() Snacks.picker.icons() end,                                                                                desc = "Icons" },
+        { "<leader>sj", function() Snacks.picker.jumps() end,                                                                                desc = "Jumps" },
+        { "<leader>sm", function() Snacks.picker.marks() end,                                                                                desc = "Marks" },
+        { "<leader>sM", function() Snacks.picker.man() end,                                                                                  desc = "Man Pages" },
+        { "<leader>sp", function() Snacks.picker.lazy() end,                                                                                 desc = "Search for Plugin Spec" },
+        { "<leader>sq", function() Snacks.picker.qflist() end,                                                                               desc = "Quickfix List" },
+        { "<leader>su", function() Snacks.picker.undo() end,                                                                                 desc = "Undo History" },
+        { "<leader>uC", function() Snacks.picker.colorschemes() end,                                                                         desc = "Colorschemes" },
+        -- LSP
+        { "gd",         function() Snacks.picker.lsp_definitions() end,                                                                      desc = "Goto Definition" },
+        { "gD",         function() Snacks.picker.lsp_declarations() end,                                                                     desc = "Goto Declaration" },
+        { "<leader>lr", function() Snacks.picker.lsp_references() end,                                                                       nowait = true,                     desc = "References" },
+        { "gi",         function() Snacks.picker.lsp_implementations() end,                                                                  desc = "Goto Implementation" },
+        { "gy",         function() Snacks.picker.lsp_type_definitions() end,                                                                 desc = "Goto T[y]pe Definition" },
+        { "<leader>ss", function() Snacks.picker.lsp_symbols() end,                                                                          desc = "LSP Symbols" },
+    },
+    init = function()
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "VeryLazy",
+            callback = function()
+                -- Setup some globals for debugging (lazy-loaded)
+                _G.dd = function(...)
+                    Snacks.debug.inspect(...)
+                end
+                _G.bt = function()
+                    Snacks.debug.backtrace()
+                end
+                vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+                -- Create some toggle mappings
+                Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+                Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+                Snacks.toggle.diagnostics():map("<leader>ud")
+                Snacks.toggle.indent():map("<leader>ug")
+                Snacks.toggle.dim():map("<leader>uD")
+            end,
+        })
+    end,
+}
