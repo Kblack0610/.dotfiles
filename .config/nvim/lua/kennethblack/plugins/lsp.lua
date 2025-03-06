aa9c7419d10bf449666efa593b51f6cedae9fc73
Kenneth <kblack0610@gmail.com>
2025-03-06 12:47:06 -0800

    set up omnisharp extended

--- .config/nvim/lua/kennethblack/plugins/lsp.lua
+++ .config/nvim/lua/kennethblack/plugins/lsp.lua
@@ -53,10 +53,12 @@ return {
       vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
         border = "rounded",
       })
-
+      local rounded_border_handlers = {
+        ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+        ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
+      }
       local pid = vim.fn.getpid()
       local omnisharp_bin = "/opt/omnisharp-roslyn/run"
-      -- Setup up all language servers that are installed
       for _, value in ipairs(ensure_installed) do
         if value == "ts_ls" then
           lspconfig[value].setup {
@@ -88,23 +90,61 @@ return {
               debounce_text_changes = 150,
             },
             cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) },
-            -- handlers = vim.tbl_extend("force", rounded_border_handlers, {
-            --   ["textDocument/definition"] = require("omnisharp_extended").handler,
-            -- }),
+            -- --cmd = { "dotnet", "/path/to/omnisharp/OmniSharp.dll" },
+            handlers = vim.tbl_extend("force", rounded_border_handlers, {
+              ["textDocument/definition"] = require("omnisharp_extended").handler,
+            }),
             settings = {
-              omnisharp = {
-                analyzeOpenDocumentsOnly = true,
-                enableAsyncCompletion = false,
-                enableMsBuildLoadProjectsOnDemand = false,
-                projectLoadTimeout = 300,
-                useModernNet = true,
-                enableRoslynAnalyzers = false,
+              FormattingOptions = {
+                -- Enables support for reading code style, naming convention and analyzer
+                -- settings from .editorconfig.
+                EnableEditorConfigSupport = true,
+                -- Specifies whether 'using' directives should be grouped and sorted during
+                -- document formatting.
+                OrganizeImports = nil,
               },
+              MsBuild = {
+                -- If true, MSBuild project system will only load projects for files that
+                -- were opened in the editor. This setting is useful for big C# codebases
+                -- and allows for faster initialization of code navigation features only
+                -- for projects that are relevant to code that is being edited. With this
+                -- setting enabled OmniSharp may load fewer projects and may thus display
+                -- incomplete reference lists for symbols.
+                LoadProjectsOnDemand = nil,
+              },
+              RoslynExtensionsOptions = {
+                -- Enables support for roslyn analyzers, code fixes and rulesets.
+                EnableAnalyzersSupport = nil,
+                -- Enables support for showing unimported types and unimported extension
+                -- methods in completion lists. When committed, the appropriate using
+                -- directive will be added at the top of the current file. This option can
+                -- have a negative impact on initial completion responsiveness,
+                -- particularly for the first few completion sessions after opening a
+                -- solution.
+                EnableImportCompletion = nil,
+                -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+                -- true
+                AnalyzeOpenDocumentsOnly = nil,
+              },
+              Sdk = {
+                -- Specifies whether to include preview versions of the .NET SDK when
+                -- determining which version to use for project loading.
+                IncludePrereleases = true,
+              },
+              -- omnisharp = {
+              --   analyzeOpenDocumentsOnly = true,
+              --   enableAsyncCompletion = false,
+              --   enableMsBuildLoadProjectsOnDemand = false,
+              --   projectLoadTimeout = 300,
+              --   useModernNet = true,
+              --   enableRoslynAnalyzers = false,
+              -- },
             },
           }
         else
           lspconfig[value].setup {
             capabilities = capabilities,
+
           }
         end
       end
@@ -143,3 +183,15 @@ return {
     end,
   },
 }
+
+-- https://github.com/omnisharp/omnisharp-roslyn
+-- OmniSharp server based on Roslyn workspaces
+--
+-- `omnisharp-roslyn` can be installed by downloading and extracting a release from [here](https://github.com/OmniSharp/omnisharp-roslyn/releases).
+-- OmniSharp can also be built from source by following the instructions [here](https://github.com/omnisharp/omnisharp-roslyn#downloading-omnisharp).
+--
+-- OmniSharp requires the [dotnet-sdk](https://dotnet.microsoft.com/download) to be installed.
+--
+-- **By default, omnisharp-roslyn doesn't have a `cmd` set.** This is because nvim-lspconfig does not make assumptions about your path. You must add the following to your init.vim or init.lua to set `cmd` to the absolute path ($HOME and ~ are not expanded) of the unzipped run script or binary.
+--
+-- For `go_to_definition` to work fully, extended `textDocument/definition` handler is needed, for example see [omnisharp-extended-lsp.nvim](https://github.com/Hoffs/omnisharp-extended-lsp.nvim)
