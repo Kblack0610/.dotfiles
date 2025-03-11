464681c22ccad8266099ec7e63961d7806cddc2b
Kenneth <kblack0610@gmail.com>
2025-03-10 19:20:46 -0700

    updates to theme

--- .local/bin/screen_power_manager/screen_power_manager.sh
+++ .local/bin/screen_power_manager/screen_power_manager.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+if [ "$(whoami)" != "root" ]; then
+    echo "Please run as root"
+    exit 1
+fi
+
+# Check if the screen saver and power saving features are enabled
+if xset q | grep -q 'DPMS is Enabled'; then
+    echo "Screen saver and power saving features are enabled"
+else
+    echo "Screen saver and power saving features are disabled"
+fi
 
 # Function to disable screen saver and power saving
 disable_screen_saver() {
