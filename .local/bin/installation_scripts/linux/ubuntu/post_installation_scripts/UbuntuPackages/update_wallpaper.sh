3c855ed65574a02a012037c1cc98510e05f7cc57
Kenneth <kblack0610@gmail.com>
2025-03-11 15:56:00 -0700

    may have fixed crontabs and added screensaver, we'll see

--- .local/bin/installation_scripts/linux/ubuntu/post_installation_scripts/UbuntuPackages/update_wallpaper.sh
+++ .local/bin/installation_scripts/linux/ubuntu/post_installation_scripts/UbuntuPackages/update_wallpaper.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-export DISPLAY=:0.0
 feh --bg-max --randomize /home/kblack0610/Media/Wallpapers/*
--- .local/bin/installation_scripts/linux/ubuntu/update_wallpaper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export DISPLAY=:0.0
-feh --bg-max --randomize /home/kblack0610/Media/Wallpapers/*
