6633d69a261ab48f591341f97931b646464e591c
Kenneth <kblack0610@gmail.com>
2025-03-11 22:44:44 -0700

    started frizbee_dir_search, have to look into using param

---
+++ .local/bin/term_scripts/frizbee/frizbee_dir_search/target/debug/deps/frizbee_dir_search-a9b785a325fba172.d
@@ -0,0 +1,5 @@
+/home/kblack0610/.dotfiles/.local/bin/term_scripts/frizbee/frizbee_dir_search/target/debug/deps/libfrizbee_dir_search-a9b785a325fba172.rmeta: src/main.rs
+
+/home/kblack0610/.dotfiles/.local/bin/term_scripts/frizbee/frizbee_dir_search/target/debug/deps/frizbee_dir_search-a9b785a325fba172.d: src/main.rs
+
+src/main.rs:
