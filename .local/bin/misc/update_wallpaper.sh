22a8e5e9bdbdc764c75543aa3da9128824402f8d
Kenneth <kblack0610@gmail.com>
2025-03-13 09:47:54 -0700

    update

--- .local/bin/misc/update_wallpaper.sh
+++ .local/bin/misc/update_wallpaper.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# in the future to add fade in transitions:
+# https://unix.stackexchange.com/questions/58224/is-there-a-way-to-make-feh-fade-in-between-transitions
 # DISPLAY=:0
 feh --bg-max --randomize /home/kblack0610/Media/Wallpapers/*
 echo "done"
