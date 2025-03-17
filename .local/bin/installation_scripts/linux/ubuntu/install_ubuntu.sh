81db1ee410c9687a88f7e5e3da5a72c382e4dfeb
Kenneth <kblack0610@gmail.com>
2025-03-17 16:45:25 -0700

    clean up install ubuntu

--- .local/bin/installation_scripts/linux/ubuntu/install_ubuntu.sh
+++ .local/bin/installation_scripts/linux/ubuntu/install_ubuntu.sh
@@ -2,14 +2,14 @@
 . ./post_installation_scripts/install_requirements_functions.sh
 pids=""
 failures=0
-(install_reqs) && \\ 
-(install_system_settings) && \
+(install_reqs) 
+(install_system_settings) 
 (install_stow)
-(install_zsh) && \
-(install_oh_my_zsh) && \
-(install_starship) && \
-(install_flatpak)
+(install_zsh) 
+(install_oh_my_zsh)
+(install_starship) 
 (install_dotfiles)
+(install_flatpak)
 (install_nerd_fonts)
 (install_tools) 
 (install_git)
