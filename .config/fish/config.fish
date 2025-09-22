ac9606dabbcaf6624030370b7e900311485854ce
Kenneth <kblack0610@gmail.com>
2025-09-22 09:06:00 -0700

    fish default config

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+fish_vi_key_bindings
+# cowsay "hello"
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
