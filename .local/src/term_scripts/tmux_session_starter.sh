89316d790d40747f32be66f84d003fb68f0c29ab
Kenneth <kblack0610@gmail.com>
2025-11-20 13:29:05 -0800

    cleaned up and added some cool tmux sessionsizers

---
+++ .local/src/term_scripts/tmux_session_starter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Ensure we can find user installed binaries (cargo, brew, etc)
+export PATH=$PATH:/usr/local/bin:$HOME/.local/bin:$HOME/bin
+
+# 1. Select project
+PROJECT=$(find ~/.config/tmuxinator -name "*.yml" | xargs -n 1 basename -s .yml | fzf --reverse --border --prompt='Select Project > ')
+
+# 2. Exit if cancelled
+[[ -z "$PROJECT" ]] && exit 0
+
+# 3. Switch or Start
+# If we are in tmux, tell tmuxinator to switch the client, not attach inside the popup
+if [ -n "$TMUX" ]; then
+    tmuxinator start "$PROJECT" --no-attach
+    # Manually switch to the session tmuxinator just created
+    tmux switch-client -t "$PROJECT"
+else
+    tmuxinator start "$PROJECT"
+fi
