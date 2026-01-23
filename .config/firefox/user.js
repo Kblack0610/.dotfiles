8ac606d33f5ffcfa198246aa3e430f5b2e4570dc
Kenneth <kblack0610@gmail.com>
2026-01-23 08:54:16 -0800

    telemetry and gbloat removed from firefox!! amazing

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -1,6 +1,6 @@
-// Firefox user preferences
+// Firefox user preferences - Debloated
 // This file is read on startup and applies settings automatically
-// Install: Copy to ~/.mozilla/firefox/<profile>/user.js
+// Install: Symlink to ~/.mozilla/firefox/<profile>/user.js
 
 // Enable userChrome.css customizations
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
@@ -8,13 +8,41 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 // Smoother scrolling
 user_pref("general.smoothScroll", true);
 
-// Disable pocket (sidebar clutter)
+// === TELEMETRY (disable all) ===
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("browser.discovery.enabled", false);
+
+// === POCKET (disable) ===
 user_pref("extensions.pocket.enabled", false);
 
-// Disable sponsored content on new tab
+// === URL BAR BLOAT ===
+user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
+user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false);
+user_pref("browser.urlbar.suggest.trending", false);
+user_pref("browser.urlbar.suggest.recentsearches", false);
+
+// === NEW TAB BLOAT ===
 user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
 user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
+user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeBookmarks", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeDownloads", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeVisited", false);
 
-// Privacy improvements
+// === PRIVACY ===
 user_pref("privacy.trackingprotection.enabled", true);
 user_pref("privacy.trackingprotection.socialtracking.enabled", true);
+
+// === MISC ANNOYANCES ===
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.tabs.firefox-view", false);
+user_pref("identity.fxaccounts.enabled", false);
