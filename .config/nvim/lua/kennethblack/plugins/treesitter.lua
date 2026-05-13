d74a771bd6f3adc65d569a59f3d70dc552ec19a5
Kenneth <kblack0610@gmail.com>
2026-05-13 15:25:22 -0700

    remove treesitter

--- .config/nvim/lua/kennethblack/plugins/treesitter.lua
+++ .config/nvim/lua/kennethblack/plugins/treesitter.lua
@@ -1,25 +1,4 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "main",
-  build = ":TSUpdate",
-  lazy = false,
-  config = function()
-    local parsers = {
-      "angular", "bash", "c_sharp", "css", "dockerfile", "go", "graphql",
-      "html", "javascript", "json", "lua", "markdown", "markdown_inline",
-      "proto", "regex", "scss", "sql", "toml", "tsx", "typescript", "vim",
-      "vimdoc", "yaml",
-    }
-    require("nvim-treesitter").install(parsers)
-
-    vim.api.nvim_create_autocmd("FileType", {
-      group = vim.api.nvim_create_augroup("TSHighlight", { clear = true }),
-      callback = function(args)
-        local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
-        if lang and pcall(vim.treesitter.start, args.buf, lang) then
-          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end
-      end,
-    })
-  end,
-}
+-- nvim-treesitter is archived (plugin retired April 2026).
+-- Neovim 0.12+ has treesitter built-in. Use :TSInstall to add parsers.
+-- Highlighting is auto-enabled by default via vim.treesitter.start().
+return {}
